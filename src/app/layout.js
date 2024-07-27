4ebdbfc0f351196c4e862b4283bce2f9d72f120e
adyeetya <2612adityasingh2000@gmail.com>
2024-07-27 16:19:48 +0530

    changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ import { Toaster } from 'react-hot-toast'
 import { UserProvider } from '../context/userContext'
 import { ProductProvider } from '../context/productContext'
 const inter = Inter({ subsets: ['latin'] })
-import Head from 'next/head'
+
 
 export const metadata = {
   title: "Yes'n'more - India's No. 1 Sexual Wellness Brand",
@@ -26,14 +26,45 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
+  const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'Organization',
+    name: "Yes'n'more",
+    url: 'https://www.yesnmore.com',
+    logo: 'https://www.yesnmore.com/images/logo.png',
+    description:
+      'Top Sexual Wellness Brand in India offering a wide range of products and expert advice.',
+    sameAs: [
+      'https://www.facebook.com/yesnmore',
+
+      'https://www.instagram.com/yesnmore',
+    ],
+    contactPoint: {
+      '@type': 'ContactPoint',
+      email: 'support@yesnmore.com',
+      contactType: 'Customer Service',
+      areaServed: 'IN',
+      availableLanguage: ['en', 'hi'],
+    },
+    address: {
+      '@type': 'PostalAddress',
+      streetAddress: '57, Badli Industrial Area',
+      addressLocality: 'Delhi',
+      addressRegion: 'DL',
+      postalCode: '110042',
+      addressCountry: 'IN',
+    },
+  }
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      </Head>
+  
       <body
         className={`'min-h-screen max-w-[100vw] overflow-x-hidden font-sans antialiased scroll-smooth ' ${inter.className}`}
       >
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
         <Toaster />
         <GlobalStateProvider>
           <ProductProvider>
